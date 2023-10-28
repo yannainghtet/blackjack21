38fff4c387ef598a2eb3d81a04f94dc6ebdfefe5
YanNaingHt <94881139+yannainghtet@users.noreply.github.com>
2023-10-28 14:35:50 +0630

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -8,7 +8,7 @@ window.onload = function() {
 }
 
 
-var socket = io.connect('http://127.0.0.1:5000')
+var socket = io.connect('https://edsblackjack.onrender.com:5000')
 
 
 // upon player connection, start the game
